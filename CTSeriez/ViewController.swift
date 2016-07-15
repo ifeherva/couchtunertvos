535cbcdb0c41014ba0e07e2604cb8fce2ed65450
István Fehérvári <goo.ksl@gmail.com>
2016-07-15 14:32:21 +0200

    Initial Commit

---
+++ CTSeriez/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CTSeriez
+//
+//  Created by Istvan Fehervari on 15/07/16.
+//  Copyright © 2016 Istvan Fehervari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CTSeriezTests/CTSeriezTests.swift
@@ -0,0 +1,36 @@
+//
+//  CTSeriezTests.swift
+//  CTSeriezTests
+//
+//  Created by Istvan Fehervari on 15/07/16.
+//  Copyright © 2016 Istvan Fehervari. All rights reserved.
+//
+
+import XCTest
+@testable import CTSeriez
+
+class CTSeriezTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
