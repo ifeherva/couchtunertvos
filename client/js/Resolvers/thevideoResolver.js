de4f92ed782da10a9b3dda8278094840450dd528
István Fehérvári <goo.ksl@gmail.com>
2016-07-18 23:23:53 +0200

    Added resolver for thevideo.me

---
+++ client/js/Resolvers/thevideoResolver.js
@@ -0,0 +1,36 @@
+function thevideomeResolver() {
+  this.resolve = function(url) {
+
+    var docsource = GetURLSourceSynch(url);
+    if (!docsource) return "";
+    //docsource = docsource.replace("<!DOCTYPE html>","");
+
+    var httpregex = /(https?:\/\/www.thevideo.me\/[^\s]+html)/g;
+    var matches = docsource.match(httpregex);
+
+    if (!matches || matches.length == 0) return "";
+
+    // we assume the first hit is correct
+    var domainurl = matches[0];
+    docsource = GetURLSourceSynch(domainurl);
+
+    var videofileregex = /(label:\s*'480p'\s*,\s*file:\s*'https?:\/\/[^\s]+mp4)/g;
+    matches = docsource.match(videofileregex);
+
+    if (!matches || matches.length == 0) return "";
+
+    // e.g. "file" : "http://d4412.allmyvideos.net/d/4smceyr4yq5dh6lnzhcidmuefh552wgbbxvovdmqvrguqqmrnlxt33xo3ld2yay/video.mp4?v2",
+    var fileblock = matches[0];
+    var regex = /(https?:\/\/[^\s"]+)/g;
+    matches = fileblock.match(regex);
+
+    if (matches[0] == fileblock) {
+      return "";
+    }
+
+    if (!matches || matches.length == 0) return "";
+    // we again assume the first one works
+
+    return matches[0];
+  }
+}
